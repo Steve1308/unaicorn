6c516af42792c41f9fac160e9ed7fa1a6377e24b
Esteban Sánchez González <esago_13@gmail.com>
2025-08-20 22:09:50 -0600

    Uploading all django files, including python file for training a model

---
+++ titanic_ml/titanic_ml/titanic_ml/ml_predict.py
@@ -0,0 +1,10 @@
+def prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title):
+    import pickle
+    x = [[pclass, sex, age, sibsp, parch, fare, embarked, title]]
+    randomforest = pickle.load(open("titanic_ml/titanic_model.sav", "rb"))
+    prediction = randomforest.predict(x)
+    if prediction[0] == 1:
+        final_prediction = "Survived"
+    else:
+        final_prediction = "Not Survived"
+    return final_prediction
